9b20dc8f8df09f1ff77266c15f85412de1118527
KiralyGeddon <imotionpostprod@gmail.com>
2025-07-02 01:30:36 +0200

    Add files via upload

---
+++ TSSR 0325 PROJECT/logiciels/menu_logiciels.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# Script écrit par Babak
+
+# Couleurs terminal (facultatif si utilisé sans GUI)
+rouge='\e[31m'
+vert='\e[32m'
+bleu='\e[34m'
+jaune='\e[33m'
+reset='\e[0m'
+
+# Fonction pour installer les logiciels (fictifs ici)
+installer() {
+    echo -e "${bleu}🔧 Installation de : $1${reset}"
+    echo $logiciel
+    sleep 1
+}
+installer1() {
+    echo -e "${vert}✔️  $1 installé avec succès !${reset}"
+    sleep 1.5
+}
+
+# Sous-menu Docker stylé
+sous_menu_docker() {
+    CHOIX=$(whiptail --title "🚀 Déploiement Docker Centralisé" --menu "Sélectionnez un logiciel à installer :" 20 90 10 \
+        "1" "📊  Portainer     - Interface web Docker" \
+        "2" "🖥️  GLPI          - Gestion de parc informatique"   \
+        "3" "📈  Zabbix        - Supervision réseau avancée" \
+        "4" "📡  Nagios        - Monitoring et alertes système" \
+        "5" "📞  XiVO          - Téléphonie IP (VOIP)" \
+        "6" "↩️  Retour" 3>&1 1>&2 2>&3)
+
+    case $CHOIX in
+        1) logiciel="docker" && installer $logiciel && bash /home/sam/script/TSSR/logiciels/scripts/install_$logiciel.sh && installer1 $logiciel ;;
+        2) logiciel="glpi" && installer "GLPI (Gestion de parc )" && bash /home/sam/script/TSSR/logiciels/scripts/install_$logiciel.sh && installer1 $logiciel ;;
+        3) logiciel="zabix" && installer "Zabbix (Supervision)" && bash /home/sam/script/TSSR/logiciels/scripts/install_$logiciel.sh && installer1 $logiciel ;;
+        4) logiciel="nagios" && installer "Nagios (Supervision)" && bash /home/sam/script/TSSR/logiciels/scripts/install_$logiciel.sh && installer1 $logiciel ;;
+        5) logiciel="xivo" && installer "XiVO (VOIP)" && bash /home/sam/script/TSSR/logiciels/scripts/install_$logiciel.sh && installer1 $logiciel ;;
+        6) return ;;
+        *) echo -e "${rouge}❌ Option invalide${reset}" ;;
+    esac
+}
+
+# Menu principal
+
+# Menu principal
+menu_principal(){
+while true; do
+    CHOIX_PRINCIPAL=$(whiptail --title "🧭 Menu Principal Administration" --menu "Que souhaitez-vous faire ?" 15 90 5 \
+        "1" "🛠️  Installation de logiciels via Docker"   \
+        "2" "🚪 Quitter" 3>&1 1>&2 2>&3)
+
+    case $CHOIX_PRINCIPAL in
+        1) sous_menu_docker ;;
+        2) #clear
+           echo -e "${jaune}👋 Merci d'avoir utilisé ce menu, à bientôt.${reset}"
+           exit 0 ;;
+        *) echo -e "${rouge}❌ Option invalide${reset}" ;;
+    esac
+done
+}
+
+main(){
+    chmod +x /home/sam/script/TSSR/logiciels/scripts/install_docker.sh
+    menu_principal
+}
+
+main
